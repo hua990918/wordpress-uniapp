49603edc3041c4eff57197cfcd91593595ee4742
zah <501608866@qq.com>
2023-05-07 15:30:08 +0800

    feat:我的页面-我的信息

---
+++ pages/my/components/top.vue
@@ -0,0 +1,73 @@
+<template>
+    <view class="top">
+        <view class="top-user">
+            <xCard class="top-card">
+                <u-avatar
+                    class="img"
+                    src="https://cdn.uviewui.com/uview/album/1.jpg"
+                    size="100rpx"></u-avatar>
+                <view class="top-card-login">
+                    <view class="click">点此登录</view>
+                    <view class="lv">lv</view>
+                </view>
+                <view class="top-card-info">
+                    <view>登录以后查看角色</view>
+                    <view>清楚缓存</view>
+                </view>
+            </xCard>
+        </view>
+    </view>
+</template>
+
+<script setup>
+    import xCard from '@/components/x-card/x-card.vue'
+</script>
+
+<style lang="scss" scoped>
+    .top {
+        height: 320rpx;
+        background: $uni-color-theme;
+        border-bottom-left-radius: 20rpx;
+        border-bottom-right-radius: 20rpx;
+        padding: 3%;
+
+        &-user {
+            padding-top: 120px;
+        }
+        &-card {
+            &-login {
+                display: flex;
+                .click {
+                    font-size: 42rpx;
+                    font-weight: 600;
+                    margin: 16rpx 0;
+                }
+                .lv {
+                    margin-left: 10rpx;
+                    margin-top: 30rpx;
+                    font-size: 14rpx;
+                    font-weight: 600;
+                    background: $uni-color-theme;
+                    color: #fff;
+                    width: 50rpx;
+                    height: 35rpx;
+                    text-align: center;
+                    border-radius: 10rpx;
+                }
+            }
+            &-info {
+                display: flex;
+                align-items: center;
+                justify-content: space-between;
+                font-size: 26rpx;
+                color: #747474;
+            }
+
+            .img {
+                position: absolute;
+                top: 200rpx;
+                left: 50rpx;
+            }
+        }
+    }
+</style>
--- pages/my/my.vue
+++ pages/my/my.vue
@@ -1,7 +1,20 @@
 <template>
-  <view>我的</view>
+    <view class="my">
+        <Top />
+        <view class="my-info"></view>
+    </view>
 </template>
 
-<script setup></script>
+<script setup>
+    import Top from './components/top.vue'
+</script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+    .my {
+        height: calc(100vh - 100rpx);
+        background: #f1f1f1;
+        &-info {
+            padding: 3%;
+        }
+    }
+</style>
